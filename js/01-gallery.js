df6ae35bb008ec8afc36b14e557d3c7bb07863d3
Serhii <110534352+hakuna00723@users.noreply.github.com>
2022-12-17 01:13:34 +0100

    01gallary-done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+const imagesMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+       <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+       </a>
+       </div>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", imagesMarkup);
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  // Check if this is a picture👀
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const imgOriginal = event.target.getAttribute("data-source");
+  const imgCreated = basicLightbox.create(`<img src = "${imgOriginal}"/>`);
+
+  imgCreated.show();
+});
 
 console.log(galleryItems);
